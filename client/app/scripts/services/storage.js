e50aa0e719b1e08bc976bab7e5dbbad60bc242c9
Damien Walsh <me@damow.net>
2015-11-06 19:00:09 +0000

    Add: Storage implementation for splitting and storing files

--- client/app/scripts/services/storage.js
+++ client/app/scripts/services/storage.js
@@ -8,10 +8,45 @@
  * Service in the blockswapClient.
  */
 angular.module('blockswapClient')
-  .service('BlockStorage', ['localStorageService', function (localStorage) {
+  .service('BlockStorage', ['$log', 'localStorageService', 'config', function ($log, localStorage, config) {
 
-    this.store = function (fuid, sequenceNumber, data) {
-      localStorage.set(fuid + '.' + sequenceNumber, data);
+    /**
+     * Store a single block locally.
+     *
+     * @param block
+     */
+    this.store = function (block) {
+      $log.debug('store block with fuid:', block.fuid, 'and seq:', block.seq);
+      localStorage.set(block.fuid + '-' + block.seq, JSON.stringify(block));
+    };
+
+    /**
+     * Split up a file into blocks and store them.
+     *
+     * @param name
+     * @param mimeType
+     * @param data
+     */
+    this.splitAndStore = function (name, mimeType, data) {
+
+      // Derive a file-unique ID for the file
+      var seed = CryptoJS.lib.WordArray.random(128/8);
+      var fuid = CryptoJS.SHA1(seed).toString();
+      $log.debug('created new fuid', fuid);
+
+      // Work out how many blocks we need
+      var total = Math.ceil(data.length / config.blockSize);
+
+      // Store each block
+      for (var blockSeq = 0; blockSeq < total; blockSeq ++) {
+        this.store({
+          data: data.slice(blockSeq * config.blockSize, (blockSeq + 1) * config.blockSize),
+          fuid: fuid,
+          seq: blockSeq + 1,
+          of: total
+        });
+      }
+
     };
 
   }]);
