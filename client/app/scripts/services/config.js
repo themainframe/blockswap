5318d186baf40195be5cc1937f859ee48d3d6902
Damien Walsh <me@damow.net>
2015-11-06 18:18:58 +0000

    Add: Configuration constant

---
+++ client/app/scripts/services/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name blockswapClient.config
+ * @description
+ * # config
+ * Constant in the blockswapClient.
+ */
+angular.module('blockswapClient')
+  .constant('config', {
+    blockSize: 1024
+  });
